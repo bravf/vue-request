f1aa4a2f56646ccd5498399138153e4392c66631
bravf <bravfing@126.com>
2020-06-24 14:08:35 +0800

    add: 增加VueRequest

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,75 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+  <vue-request requestKey="getData" :fetcher="getData">
+    <template v-slot="{ state, run, runFactory, cancel }">
+      <div class="search">
+        <input type="text" v-model="search.words"/>
+        <button @click="run(search.words)" :disabled="state.loading">search</button>
+        <button @click="runFactory(loadMore)(search.words)" :disabled="state.loading">search load more</button>
+        <button @click="cancel">cancel</button>
+      </div>
+      <div class="result">
+        <div v-if="state.loading">加载中。。。</div>
+        <div v-if="state.error">{{ state.error }}</div>
+        <div v-if="state.data">
+          <h2>搜索记录</h2>
+          <div v-for="query in state.data">{{query}}</div>
+        </div>
+      </div>
+    </template>
+  </vue-request>
+
+  <vue-request requestKey="getData">
+    <template v-slot="{ state, run }">
+      <div class="search">
+        <input type="text" v-model="search.words"/>
+        <button @click="run(search.words)" :disabled="state.loading">search</button>
+      </div>
+      <div class="result">
+        <div v-if="state.loading">加载中。。。</div>
+        <div v-if="state.data">{{ state.data }}</div>
+        <div v-if="state.error">{{ state.error }}</div>
+      </div>
+    </template>
+  </vue-request>
   </div>
 </template>
 
 <script>
+import VueRequest from './VueRequest.vue'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  components: {
+    VueRequest,
+  },
+  data () {
+    return {
+      search: {
+        words: '',
+      },
+    }
+  },
+  methods: {
+    loadMore (state, data) {
+      if (!state.data) {
+        state.data = data
+      }
+      else {
+        state.data = [...state.data, ...data]
+      }
+    },
+    getData (words = '') {
+      return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+          if (this.search.words === 'error') {
+            reject('网络错误')
+          }
+          else {
+            resolve([words +  ~~(Math.random() * 100)])
+          }
+        }, 1000)
+      })
+    }
+  },
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/VueRequest.vue
@@ -0,0 +1,125 @@
+<script>
+const VueRequestMap = {
+  // requestKey: {
+  //   run () => {},
+  //   cancel () => {},
+  //   fetcher () => {},
+  //   state: {
+  //     loading: false,
+  //     error: null,
+  //     data: null,
+  //   }
+  // }
+}
+export default {
+  props: {
+    requestKey: String,
+    fetcher: {
+      type: Function,
+      default: () => {
+        return Promise.resolve()
+      }
+    },
+    tag: {
+      type: String,
+      default: 'div',
+    },
+    manual: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data () {
+    return {
+      request: null,
+    }
+  },
+  methods: {
+    createRequest () {
+      const dataHandler = (state, data) => {
+        state.data = data
+      }
+      let request = {
+        fetcher: this.fetcher,
+        run: () => {},
+        cancel: () => {
+          request.state.loading = false
+        },
+        state: {
+          loading: false,
+          error: null,
+          data: null,
+        },
+      }
+      const runFactory = (handler = dataHandler) => {
+        return (...args) => {
+          const { fetcher, state } = request
+          // 处理竞争
+          if (state.loading) {
+            return
+          }
+          state.loading = true
+          fetcher(...args).then(data => {
+            // 如果被 cancel 了，不执行结果
+            if (!state.loading) {
+              return
+            }
+            handler(state, data)
+            state.loading = false
+          }).catch(err => {
+            if (!state.loading) {
+              return
+            }
+            state.error = err
+            state.loading = false
+          })
+        }
+      }
+      request = {
+        ...request,
+        run: runFactory(),
+        runFactory,
+      }
+      return request
+    },
+    getRequest () {
+      if (this.requestKey) {
+        if (this.requestKey in VueRequestMap) {
+          this.request = VueRequestMap[this.requestKey]
+        }
+        else {
+          this.request = VueRequestMap[this.requestKey] = this.createRequest()
+        }
+      }
+      else  {
+        this.request = this.createRequest()
+      }
+    },
+    getVNode (h) {
+      let $node
+      const request = this.request
+      if (this.$scopedSlots.default) {
+        $node = this.$scopedSlots.default({
+          ...request,
+        })
+      }
+      if (this.$slots.default) {
+        $node = this.$slots.default
+      }
+      if ($node && ($node.length > 1 || !$node[0].tag)) {
+        return h(this.tag, $node)
+      }
+      return $node
+    }
+  },
+  render (h) {
+    return this.getVNode(h)
+  },
+  created () {
+    this.getRequest()
+    if (!this.manual) {
+      this.request.run()
+    }
+  }
+}
+</script>
